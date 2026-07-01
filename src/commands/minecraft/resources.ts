e33147f3e9515ed3b09da9c2aa5e5e8a424e24e9
Javithor360 <floresmejia004@gmail.com>
2026-07-01 12:13:13 -0600

    feat: improve assets command

--- src/commands/minecraft/resources.ts
+++ src/commands/minecraft/resources.ts
@@ -23,11 +23,15 @@ export const execute = async (interaction: ChatInputCommandInteraction | any) =>
       iconURL: 'https://media.discordapp.net/attachments/1032440236564824105/1513754769322414080/Picel.gif?ex=6a468b3d&is=6a4539bd&hm=e55f878fd1e8908143d9560aac967a65f86a3abc72dedfe105451763a8a5d5e1&'
     })
     .setThumbnail('https://media.discordapp.net/attachments/1032440236564824105/1519191750948819025/corazonestezzlar.png?ex=6a468c52&is=6a453ad2&hm=c41a97b147fc4571a5e65543af06f4b2d14c019db05c4fb539912c82d93fa9a9&format=webp&quality=lossless&width=960&height=960&')
-    .setDescription('¡Aquí tienes todo lo necesario para unirte a la aventura! Descarga la carpeta con los mods y texturas actualizados desde el siguiente enlace:\n\n🔗 [Descargar recursos aquí](https://drive.google.com/file/d/1JPClbxD9urlw_sNY2xtu-5OD8S0YIlE5/view)\n*El archivo incluye mods, paquetes de recursos y shaders.*\n\n Si necesitas ayuda con la instalación, por favor abre un ticket en [este canal](https://discord.com/channels/707103390852710491/1406385534712156301/1521266058206511146).')
+    .setDescription('¡Aquí tienes todo lo necesario para unirte a la aventura! Descarga la carpeta con los mods y texturas actualizados desde el siguiente enlace:\n\n🔗 [Descargar recursos aquí](https://drive.google.com/file/d/1JPClbxD9urlw_sNY2xtu-5OD8S0YIlE5/view)\n*El archivo incluye mods, paquetes de recursos y shaders.*\n\n⚠️ **Requisito Importante**\nEl servidor está en la versión **Fabric 26.2** y necesitas tener **Fabric Loader** instalado. Si aún no lo tienes, puedes descargar el instalador en su última versión para la 26.2 en [este enlace](https://fabricmc.net/use/installer/).')
     .addFields(
       {
-        name: '🛠️ ¿Cómo instalar?',
+        name: '🛠️ Proceso de instalación',
         value: '>>> **1.** Descarga los recursos desde el enlace de arriba.\n**2.** Abre la carpeta de tu juego buscando `%appdata%\\.minecraft` (en Windows).\n**3.** Descomprime los recursos en esa carpeta.\n**4.** Reemplaza los archivos ya existentes.\n\n¡Y listo! Ya puedes abrir el juego y disfrutar.',
+      },
+      {
+        name: '💬 ¿Necesitas ayuda?',
+        value: 'Si necesitas ayuda con la instalación, por favor abre un ticket en [este canal](https://discord.com/channels/707103390852710491/1406385534712156301/1521266058206511146).'
       }
     )
     .setFooter({ text: 'Última actualización' })
